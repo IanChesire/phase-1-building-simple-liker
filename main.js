7eafd1548f1f20b9ba87e0a0247ad324e9d422dc
Ian <iankosgey535@gmail.com>
2024-01-14 15:10:51 +0300

    Liked and unliked

--- main.js
+++ main.js
@@ -3,9 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function() {
+  const hearts = document.querySelectorAll('.like-glyph');
 
+  hearts.forEach(heart => {
+    heart.addEventListener('click', function() {
+      mimicServerCall()
+        .then(() => {
+          if (heart.classList.contains('activated-heart')) {
+            // User unliked, revert to empty heart
+            heart.innerText = EMPTY_HEART;
+            heart.classList.remove('activated-heart');
+          } else {
+            // User liked, change to full heart
+            heart.innerText = FULL_HEART;
+            heart.classList.add('activated-heart');
+          }
+        })
+        .catch(error => {
+          // Display error modal
+          const modal = document.getElementById('modal');
+          const modalMessage = document.getElementById('modal-message');
+          modalMessage.innerText = error;
+          modal.classList.remove('hidden');
 
-
+          // Hide modal after 3 seconds
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
